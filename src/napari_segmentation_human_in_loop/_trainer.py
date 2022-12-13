1bf1d2e74c408f989eb1cde0756ac85309c8ff1b
Yohsuke Fukai <ysk@yfukai.net>
2022-12-13 15:14:10 +0900

    scaffolding ...

---
+++ src/napari_segmentation_human_in_loop/_trainer.py
@@ -0,0 +1,10 @@
+# %%
+class CellposeTrainer:
+    def __init__(self, save_path) -> None:
+        self.save_path = save_path
+
+    def train(self, images, labels) -> None:
+        pass
+
+    def pred(self, images):
+        pass
--- src/napari_segmentation_human_in_loop/_widget.py
+++ src/napari_segmentation_human_in_loop/_widget.py
@@ -6,26 +6,91 @@ see: https://napari.org/stable/plugins/guides.html?#widgets
 
 Replace code below according to your needs.
 """
+import logging
+from enum import Enum
+from glob import glob
+from os import path
 from typing import TYPE_CHECKING
 
 from magicgui import magic_factory
+from napari.utils.notifications import show_error
+from skimage.io import imread
+
+from ._trainer import CellposeTrainer
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 if TYPE_CHECKING:
     pass
 
+IMAGES_EXT = [".tiff", ".png"]
+
+
+class Trainers(Enum):
+    cellpose = CellposeTrainer
+
+
+def _check_input_folder(folder):
+    if folder == "" or not path.isdir(folder):
+        logger.warning(f"path {folder} is not a valid directory")
+        show_error(f"path {folder} is not a valid directory")
+        return False
+    else:
+        return True
+
+
+def _load_images_and_label_paths(folder):
+    logger.debug("load images and labels started")
+    image_paths = sum(
+        [glob(path.join(folder, "*" + ext)) for ext in IMAGES_EXT], []
+    )
+    if len(image_paths) == 0:
+        show_error(f"no image was found in path {folder}")
+        return [], [], []
+    logger.debug(f"found {len(image_paths)} images")
+    label_paths = [f + ".label.npy" for f in image_paths]
+    image_with_label_paths, new_label_paths = zip(
+        [(f, lf) for f, lf in zip(image_paths, label_paths) if path.isfile(lf)]
+    )
+    image_without_label_paths = [
+        f for f, lf in zip(image_paths, label_paths) if not path.isfile(lf)
+    ]
+    logger.debug("load images and labels finished")
+    return image_with_label_paths, image_without_label_paths, new_label_paths
+
 
 @magic_factory(
-    train_partial=dict(
-        widget_type="PushButton",
-        text="Train partial",
-        tooltip="partially train the model using the current image / label",
+    input_folder=dict(
+        widget_type="FileEdit",
+        mode="d",
+        label="custom model path: ",
+        tooltip="if model type is custom, specify file path to it here",
     ),
-    train_all=dict(
-        widget_type="PushButton",
-        text="Train all",
-        tooltip="train the model using the all images / labels in directory",
-    ),
-    call_button=False,
+    call_button="Training",
 )
-def wizard_widget(train_partial, train_all):
-    pass
+def wizard_widget(
+    input_folder, train_all: bool, trainer_cls: Trainers = Trainers.cellpose
+):
+    if not _check_input_folder(input_folder):
+        return
+    logger.debug("training called")
+    (
+        image_with_label_paths,
+        image_without_label_paths,
+        label_paths,
+    ) = _load_images_and_label_paths(input_folder)
+
+    if train_all:
+        images = [imread(f) for f in image_with_label_paths]
+        labels = [imread(f) for f in label_paths]
+    else:
+        image = [...]
+        label = [...]
+
+    trainer = trainer_cls.value()
+    trainer.train(image, label)
+
+    if len(image_without_label_paths) > 0:
+        new_image = image_without_label_paths[0]
+        new_label = trainer.predict(new_image)
