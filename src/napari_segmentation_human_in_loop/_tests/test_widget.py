479fb17412af6c47d7ffed6e25f7f010adfa3fd8
Yohsuke Fukai <ysk@yfukai.net>
2022-12-21 11:08:29 +0900

    added torch in pytest

--- src/napari_segmentation_human_in_loop/_tests/test_widget.py
+++ src/napari_segmentation_human_in_loop/_tests/test_widget.py
@@ -1,5 +1,15 @@
 import pytest
 
+# https://github.com/MouseLand/cellpose-napari/pull/35 thanks @psobolewskiPhD!
+import torch  # noqa: F401
+
+from napari_segmentation_human_in_loop._widget import (
+    IMAGES_LAYER_NAME,
+    LABELS_LAYER_NAME,
+    Trainers,
+    wizard_widget,
+)
+
 
 @pytest.mark.parametrize(
     "training",
@@ -11,22 +21,21 @@ def test_example_magic_widget(make_napari_viewer, datadir, training):
     viewer = make_napari_viewer()
     # this time, our widget will be a MagicFactory or FunctionGui instance
 
+    my_widget = wizard_widget()
 
-#    my_widget = wizard_widget()
-#
-#    # if we "call" this object, it'll execute our function
-#    my_widget(
-#        viewer,
-#        datadir,
-#        "test2",
-#        training=training,
-#        trainer_cls=Trainers.cellpose,
-#    )
-#
+    # if we "call" this object, it'll execute our function
+    my_widget(
+        viewer,
+        datadir,
+        "test2",
+        training=training,
+        trainer_cls=Trainers.cellpose,
+    )
 
-#    assert IMAGES_LAYER_NAME in viewer.layers
-#    assert LABELS_LAYER_NAME in viewer.layers
-#
+    assert IMAGES_LAYER_NAME in viewer.layers
+    assert LABELS_LAYER_NAME in viewer.layers
+
+
 #    # read captured output and check that it's as we expected
 #    # captured = capsys.readouterr()
 #
